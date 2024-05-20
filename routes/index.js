48ea54de0028e7c5ad74745d4ff46bb92afbfb23
Adem <ademduran6888@gmail.com>
2024-05-21 00:23:01 +0200

    fix(routes): updating import and editing api route corresponding to configsRoutes

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ const auth = require("../middlewares/auth");
 
 const authRoutes = require("./auth");
 const typeRoutes = require("./type");
-const configRoutes = require("./type");
+const configRoutes = require("./config");
 
 router.use("/", authRoutes);
 router.use("/types", typeRoutes);
-router.use("/config", configRoutes);
+router.use("/configs", configRoutes);
 
 module.exports = router;
